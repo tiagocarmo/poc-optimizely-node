3a745caf421d741022745bdf74cc408e1fdb174e
Tiago Carmo <contato@tiagocarmo.com.br>
2021-01-07 18:53:55 -0300

    remove key

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const optimizelySDK = require('@optimizely/optimizely-sdk');
 const v4 = require('uuid');
 
 const optimizelyClientInstance = optimizelySDK.createInstance({
-  sdkKey: 'Y2j1KVANegm8ES25QjZDQ'
+  sdkKey: '<sdk-key>'
 });
 
-const user = { id: v4() }; // identificação do usuário é obrigatória (parece)
+const user = { id: v4() };
 const attributes = {};
 
 optimizelyClientInstance.onReady().then(() => {
@@ -21,8 +21,4 @@ optimizelyClientInstance.onReady().then(() => {
 
   // se não fechar a conexão, o app não fecha
   optimizelyClientInstance.close();
-
-  // tem um cache bizarro, demora quase 1 minuto dependendo.
-  // altera aqui:
-  // https://app.optimizely.com/v2/projects/19589593569/features/19753116415/rules
 });
